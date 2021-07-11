2abc9baf452adc9428c66a9e6cc604f2b6234c72
JCevil <jugamont@espol.edu.ec>
2021-07-11 17:59:55 -0500

    Act archivos

---
+++ src/main/java/ec/edu/espol/util/Util.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.util;
+
+import java.io.File;
+import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException; 
+
+/**
+ *
+ * @author juanc
+ */
+public class Util {
+    
+    private Util(){
+    }
+    
+    public static int nextID(String nomfile)
+    {
+        int id = 0;
+        try(Scanner sc = new Scanner(new File(nomfile)))
+        {
+           while(sc.hasNextLine())
+           {
+               String linea = sc.nextLine();
+               String[] tokens = linea.split("\\|");
+               id = Integer.parseInt(tokens[0]);
+           }
+        }
+        catch(Exception e)
+        {
+        }
+        return id+1;
+    }
+    public static String convertirSHA256(String password) {
+	MessageDigest md = null;
+	try {
+		md = MessageDigest.getInstance("SHA-256");
+	} 
+	catch (NoSuchAlgorithmException e) {		
+		e.printStackTrace();
+		return null;
+	}
+	    
+	byte[] hash = md.digest(password.getBytes());
+	StringBuilder sb = new StringBuilder();
+	    
+	for(byte b : hash) {        
+		sb.append(String.format("%02x", b));
+	}
+	    
+	return sb.toString();
+    }
+}
